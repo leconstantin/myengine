06959e884f52c13feafddb167193bea8e5980349
MUHIRWE Leo Constantin <142350704+le250123@users.noreply.github.com>
2024-12-02 17:23:23 +0200

    Lecon:added search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <HomeHeader />
+      <section className="flex flex-col items-center mt-24">
+        <Image
+          src="/google.webp"
+          alt="Google Logo"
+          width={300}
+          height={100}
+          priority
+          style={{ width: "auto" }}
+        />
+        <HomeSearch />
+      </section>
     </main>
   );
 }
---
+++ src/components/HomeSearch.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { use, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import { useRouter } from "next/navigation";
+
+export default function HomeSearch() {
+  const [input, setInput] = useState("");
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  };
+
+  const randomSearch = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    setRandomSearchLoading(true);
+    const response = await fetch("https://random-word-api.herokuapp.com/word")
+      .then((res) => res.json())
+      .then((data) => data[0]);
+    if (!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full mt-5 max-w-[90%] mx-auto border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition sm:max-w-xl lg:max-w-2xl"
+      >
+        <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
+        <input
+          type="text"
+          className="flex-grow focus:outline-none"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <BsFillMicFill className="text-lg" />
+      </form>
+      <div className="flex flex-col space-y-2 sm:space-y-0 justify-center sm:flex-row mt-8 sm:space-x-4">
+        <button
+          className="bg-[#f8f9fa] rounded-md text-sm text-gr hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow"
+          onClick={handleSubmit}
+        >
+          Google search
+        </button>
+        <button
+          className="bg-[#f8f9fa] rounded-md text-sm text-gr hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow"
+          onClick={randomSearch}
+        >
+          {randomSearchLoading ? "Loading..." : "I am feeling lucky"}
+        </button>
+      </div>
+    </>
+  );
+}
