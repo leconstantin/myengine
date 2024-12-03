bef30e2d0b47bf0bf177697bf7e36028104f1f9c
MUHIRWE Leo Constantin <142350704+le250123@users.noreply.github.com>
2024-12-03 12:10:55 +0200

    Lecon: worked on all and images pages

--- src/app/search/image/page.tsx
+++ src/app/search/image/page.tsx
@@ -1,3 +1,34 @@
-export default function Image() {
-  return <div className="text-red-500">Image</div>;
+import ImageSearchResults from "@/components/ImageSearchResults";
+import WebSearchResults from "@/components/WebSearchResult";
+
+import Link from "next/link";
+
+export default async function ImageSearchPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+
+  if (!response.ok) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No result found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <div>{results && <ImageSearchResults results={data} />}</div>;
 }
