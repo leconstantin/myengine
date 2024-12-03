c2645a82b701cc135c82e0954daf700272145b38
MUHIRWE Leo Constantin <142350704+le250123@users.noreply.github.com>
2024-12-03 14:04:13 +0200

    removed promises

--- src/app/search/image/page.tsx
+++ src/app/search/image/page.tsx
@@ -9,7 +9,6 @@ export default async function ImageSearchPage({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const startIndex = searchParams.start || "1";
-  await new Promise((resolve) => setTimeout(resolve, 10000));
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
   );
