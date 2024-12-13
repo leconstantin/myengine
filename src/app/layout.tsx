e059a9316f5434d6d0c92cbb82574f623f3cb479
MUHIRWE Leo Constantin <142350704+le250123@users.noreply.github.com>
2024-12-13 14:52:25 +0200

    returned back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Constantin",
+  title: "My Google",
   description:
     "Google clone created by Lecon using tailwind react nextjs and more",
 };
